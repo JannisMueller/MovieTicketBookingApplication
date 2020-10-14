8295355c9ba36da82ef67283621c4f5e1056019f
albert.andersson <andersson.albert@gmail.com>
2020-10-14 10:44:38 +0200

    Added Customer.java and customer.db

---
+++ src/sample/Customer.java
@@ -0,0 +1,41 @@
+package sample;
+
+public class Customer {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main extends Application {
 
     @Override
@@ -15,9 +20,35 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
+
+        final String DB_NAME = "customer.db";
+        final String CONNECTION_STRING = "jdbc:sqlite:file:src\\" + DB_NAME;
+        final String TABLE_CUSTOMER = "customer";
+
+        final String COLUMN_FIRSTNAME = "firstname";
+        final String COLUMN_LASTNAME = "lastname";
+        final String COLUMN_EMAIL = "email";
+        final String COLUMN_PHONE = "phone";
+
+        try (Connection conn = DriverManager.getConnection(CONNECTION_STRING);
+
+             Statement statement = conn.createStatement()) {
+
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CUSTOMER +
+                    " (" + COLUMN_FIRSTNAME + " text, " +
+                    COLUMN_LASTNAME + " text, " +
+                    COLUMN_EMAIL + " text, " +
+                    COLUMN_PHONE + " text" +
+                    ")");
+
+        } catch(
+                SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+            e.printStackTrace();
+        }
+
         launch(args);
     }
 }
