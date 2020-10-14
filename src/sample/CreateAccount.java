181b24cdd2e5fa4fd8b5c1dc0833ce905bc25fdb
albert.andersson <andersson.albert@gmail.com>
2020-10-14 13:12:56 +0200

    Updated method for creatingCustomer

--- src/sample/CreateAccount.java
+++ src/sample/CreateAccount.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -11,12 +15,16 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class CreateAccount extends Application {
     public void start(Stage stage) {
 
-        Text tHeader = new Text("Enter your user details");
+        Text tHeader = new Text("Create account");
         tHeader.setFont(Font.font("", FontWeight.BOLD, 20));
 
         // First name
@@ -40,6 +48,15 @@ public class CreateAccount extends Application {
         TextField tfPhone = new TextField();
         tfPhone.setPromptText("Phone");
 
+        // Buttons
+        Button btnCreateAccount = new Button("Create Account");
+        Button btnBack = new Button("Back");
+
+
+        // Password
+        Label lblPassword = new Label("Password: ");
+        PasswordField pfPassword = new PasswordField();
+
         // Creating and formatting gridpane
         GridPane gridPane = new GridPane();
         gridPane.setVgap(10);
@@ -51,14 +68,47 @@ public class CreateAccount extends Application {
         gridPane.add(lblFirstName, 0, 1);
         gridPane.add(tfFirstName, 1, 1);
 
-        gridPane.add(lblLastName, 0,2);
-        gridPane.add(tfLastName,1,2);
+        gridPane.add(lblLastName, 0, 2);
+        gridPane.add(tfLastName, 1, 2);
+
+        gridPane.add(lblEmail, 0, 3);
+        gridPane.add(tfEmail, 1, 3);
+
+        gridPane.add(lblPhone, 0, 4);
+        gridPane.add(tfPhone, 1, 4);
+
+        gridPane.add(lblPassword, 0, 5);
+        gridPane.add(pfPassword, 1, 5);
+
+        gridPane.add(btnCreateAccount, 1, 6);
+        gridPane.add(btnBack, 0, 6);
+
+        btnCreateAccount.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                try (Connection conn = DriverManager.getConnection(Main.CONNECTION_STRING);
+                     Statement statement = conn.createStatement()) {
+                    String firstname = tfFirstName.getText();
+                    String lastname = tfLastName.getText();
+                    String email = tfEmail.getText();
+                    String phone = tfPhone.getText();
+                    String password = pfPassword.getText();
+
+                    Main.createCustomer(statement, firstname, lastname, email, phone, password);
+
+                } catch (
+                        SQLException e) {
+                    System.out.println("Something went wrong: " + e.getMessage());
+                    e.printStackTrace();
+                }
 
 
 
 
-
-
+            }
+        });
 
 
         Scene scene2 = new Scene(gridPane, 400, 400);
